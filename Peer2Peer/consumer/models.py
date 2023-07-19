c02ab893572f2d269b98c3054fccfe29baa0d386
ShivamMalpani <shivammalpani@iitbhilai.ac.in>
2023-07-19 17:47:55 +0530

    added Container, Newsletter model

--- Peer2Peer/consumer/models.py
+++ Peer2Peer/consumer/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Products(models.Model):
+class Products(models.Model): #sql
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=10000)
     seller = models.CharField(max_length=100) # sellerid
@@ -17,15 +17,16 @@ class Products(models.Model):
     def __str__(self):
         return self.title
 
-class Reviews(models.Model):
+class Reviews(models.Model): # sql/nosql
     productID = models.CharField()
     userID = models.CharField()
     review = models.CharField()
     time = models.DateTimeField()
-
+    # add edit field later
+    # edit = models.BooleanField(default=False)
     def __str__(self):
         return self.title
-class Ratings(models.Model):
+class Ratings(models.Model): # nosql
     productID = models.CharField()
     userID = models.CharField()
     ratings = models.CharField()
@@ -34,20 +35,31 @@ class Ratings(models.Model):
     def __str__(self):
         return self.title
 
-class Comments(models.Model):
-    productID = models.CharField()
-    userID = models.CharField()
-    comments = models.CharField()
-    time = models.DateTimeField()
-    edit = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
 
-class Cart(models.Model):
+class Cart(models.Model): # nosql/sql
     productID = models.CharField()
     userID = models.CharField()
     price = models.IntegerField()
     discount = models.IntegerField()
     quantity = models.IntegerField()
+
+
+class Container(models.Model): # nosql
+    productID = models.CharField()
+    ContainerID = models.CharField() # use UUIDField
+    price = models.IntegerField()
+    discount = models.IntegerField()
+    quantity = models.IntegerField()
+
+class User(models.Model): # sql
+    name = models.CharField()
+
+
+class Newsletter(models.Model): # sql
+    userID = models.CharField()
+    emailID = models.EmailField()
+
 
