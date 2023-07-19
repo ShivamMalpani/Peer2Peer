ae874d6ca911b5beefa289d64233c471f0c334be
ShivamMalpani <shivammalpani@iitbhilai.ac.in>
2023-07-19 15:19:55 +0530

    added Comments, Cart model

--- Peer2Peer/consumer/models.py
+++ Peer2Peer/consumer/models.py
@@ -8,12 +8,14 @@ class Products(models.Model):
     create_time = models.DateTimeField()
     last_updated = models.DateTimeField()
     # image =
-    cost = models.IntegerField()
+    price = models.IntegerField()
     discount = models.IntegerField(default=0)
     customer_count = models.IntegerField()
     stock = models.IntegerField()
     category = models.CharField()
 
+    def __str__(self):
+        return self.title
 
 class Reviews(models.Model):
     productID = models.CharField()
@@ -21,13 +23,31 @@ class Reviews(models.Model):
     review = models.CharField()
     time = models.DateTimeField()
 
+    def __str__(self):
+        return self.title
 class Ratings(models.Model):
     productID = models.CharField()
     userID = models.CharField()
-    review = models.CharField()
+    ratings = models.CharField()
     time = models.DateTimeField()
 
     def __str__(self):
         return self.title
 
+class Comments(models.Model):
+    productID = models.CharField()
+    userID = models.CharField()
+    comments = models.CharField()
+    time = models.DateTimeField()
+    edit = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+class Cart(models.Model):
+    productID = models.CharField()
+    userID = models.CharField()
+    price = models.IntegerField()
+    discount = models.IntegerField()
+    quantity = models.IntegerField()
 
